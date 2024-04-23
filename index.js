b42787634e5e95392b81261752f53cc243f21706
Antim <antimsarker0@gmail.com>
2024-04-23 08:51:18 +0600

    cors solving

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8qmgmso.mongodb.net/movieList?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -64,9 +67,7 @@ app.delete('/movielist/:id', async (req, res) => {
 }
 run().catch(console.dir);
 
-//middleware
-app.use(cors())
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('Hello bhai!')
